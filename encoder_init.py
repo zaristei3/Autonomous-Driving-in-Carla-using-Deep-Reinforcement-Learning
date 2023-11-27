f476ad383a3d4a830cd59a3e9631d40140b253be
Zachary Aristei <zaristei3@gatech.edu>
2023-11-27 02:17:03 +0000

    fix 2

--- encoder_init.py
+++ encoder_init.py
@@ -24,17 +24,23 @@ class EncodeState():
         image_obs = torch.tensor(observation[0], dtype=torch.float).to(self.device)
         image_obs = image_obs.unsqueeze(0)
         image_obs = image_obs.permute(0,3,2,1)
-        image_obs = self.conv_encoder(image_obs)
+        image_obs_result = self.conv_encoder(image_obs)
+
+        for t in range(100):
+            if not torch.any(torch.isinf(image_obs_result)):
+                break
+            else:
+                image_obs_result = self.conv_encoder(image_obs)
 
         if torch.any(torch.isinf(image_obs)):
-            im = Image.fromarray(observation[0])
+            im = Image.fromarray(observation[0].reshape(observation[0].shape[1], observation[0].shape[0], observation[0].shape[2]))
             im.save('err_img.png')
             raise RuntimeError
         else:
-            im = Image.fromarray(observation[0])
+            im = Image.fromarray(observation[0].reshape(observation[0].shape[1], observation[0].shape[0], observation[0].shape[2]))
             im.save('img.png')
 
         navigation_obs = torch.tensor(observation[1], dtype=torch.float).to(self.device)
-        observation = torch.cat((image_obs.view(-1), navigation_obs), -1)
+        observation = torch.cat((image_obs_result.view(-1), navigation_obs), -1)
         
         return observation
